3bc9e7e82397ae684e9803c437e1d2e126cc60cb
Rob Toombs <toombs.rob@gmail.com>
2020-05-05 03:45:34 +0100

    Finally starting to figure out Postgres, SpringBoot, and IntelliJ...

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql:42.2.12")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
