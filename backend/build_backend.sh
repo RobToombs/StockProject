7ed43c894c77b3f6a1d4a14912ee4021efabf2b3
Rob Toombs <toombs.rob@gmail.com>
2020-05-31 04:07:21 +0100

    Missed some files

--- backend/build_backend.sh
+++ backend/build_backend.sh
@@ -5,6 +5,9 @@
 #docker rm backend-app
 #docker rmi backend-app
 
+## Create our backend jar
+gradle bootJar
+
 ## Create the build directory structure
 mkdir -p ./build/dependency && (cd ./build/dependency; jar -xf ../libs/*.jar)
 
